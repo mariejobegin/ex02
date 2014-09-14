8dbe69a3f29dabca06413b3811e9addb645f84ef
Dzenan Ridjanovic <dzenanr@gmail.com>
2014-09-14 11:09:38 +0200

    add dr.txt

--- bin/ex02.dart
+++ bin/ex02.dart
@@ -1,19 +1,18 @@
 import 'dart:math' as math;
 
 void main() {
-  int x;
-  int y;
-  int z;
   // Question 1
-  x = 5;
-  y = 7;
-  z = 8;
+  int x = 5;
+  int y = 7;
+  int z = 8;
   // En calculant à la main j'avais comme résultat 1/4 tandis que Dart donne 4.125 dû à la priorité des opérateurs
   print ("Réponse question no. 1 :");
   print (x-y/z);
   // Question 2
   print ("Réponse question no. 2 :");
-  print( 2 * math.PI * 10);
+  //print( 2 * math.PI * 10);
+  print(math.PI * math.pow(10, 2));
+  // https://api.dartlang.org/apidocs/channels/stable/dartdoc-viewer/dart-math#id_pow
   // Question 3
   int a = 43560;
   print ("Réponse question no. 3 :");
